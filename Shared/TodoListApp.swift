5fe747c94e62e698645940815669bee68a2ac0a8
jiho lee <optional.int@kakao.com>
2021-03-07 20:09:21 +0900

    Initial Commit

---
+++ Shared/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  Shared
+//
+//  Created by jiho lee on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
